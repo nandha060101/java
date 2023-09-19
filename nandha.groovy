95a95de53198fc55ddcf70d1890ec8aae97eb1cf
nandha060101 <143711642+nandha060101@users.noreply.github.com>
2023-09-19 22:00:49 +0530

    Update nandha.groovy

--- nandha.groovy
+++ nandha.groovy
@@ -1,1 +1,28 @@
-echo "nandha"
+properties([
+    parameters([
+        choice(
+            choices: ['demo'],
+            description: 'Select branch to build',
+            name: 'branch')])])
+                        node{
+                                    if ("${params.branch}" == 'demo')
+                    {
+                       stage("Code checkout from SCM")
+                        {
+							checkout([
+							$class: 'GitSCM',
+							branches: [[name: '*/demo']],
+							doGenerateSubmoduleConfigurations: false,
+							extensions: [],
+							submoduleCfg: [],
+							userRemoteConfigs: [[
+							credentialsId: 'none',
+							url: 'https://github.com/nandha060101/java.git']]])
+echo nandha.groovy
+
+                        }
+                       
+                    
+                    }
+            }
+                   
