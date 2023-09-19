f71afefce15d8c97ee5f80b04b2dc2970955637a
nandha060101 <143711642+nandha060101@users.noreply.github.com>
2023-09-19 22:20:48 +0530

    Update nandha.groovy

--- nandha.groovy
+++ nandha.groovy
@@ -11,7 +11,7 @@ properties([
                         {
 							checkout([
 							$class: 'GitSCM',
-							branches: [[name: '/demo']],
+							branches: [[name: '/main']],
 							doGenerateSubmoduleConfigurations: false,
 							extensions: [],
 							submoduleCfg: [],
